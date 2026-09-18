05a85e6148c41a22f275fcf08f807374d910000a
李大壮 <lihe6666@163.com>
2026-09-18 06:47:07 -0400

    fix(import): accept HTML bookmark exports misdetected as application/javascript
    Browser HTML bookmark exports (Netscape format) can be detected by libmagic
    as application/javascript when they contain javascript: bookmarklets. Laravel's
    mimes:html,htm rule validates via MIME-to-extension guessing and therefore
    rejects the file with "must be a file of type: html, htm". Switch to the
    extensions:html,htm rule, which validates the uploaded file name and is robust
    against MIME misdetection.

--- app/Http/Requests/DoImportRequest.php
+++ app/Http/Requests/DoImportRequest.php
@@ -12,7 +12,7 @@ class DoImportRequest extends FormRequest
             'import-file' => [
                 'required',
                 'file',
-                'mimes:html,htm',
+                'extensions:html,htm',
             ],
         ];
     }
